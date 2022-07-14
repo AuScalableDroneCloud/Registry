4d8e088091bb560906f8e7404d1282a2b1aeedfa
Owen Kaluza <owen.kaluza@monash.edu>
2022-07-14 18:34:54 +1000

    Attempt a fix

--- Registry.Web/Controllers/AccountController.cs
+++ Registry.Web/Controllers/AccountController.cs
@@ -1,45 +1,69 @@
 // Controllers/AccountController.cs
 // https://github.com/auth0/auth0-aspnetcore-authentication
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Registry.Ports.DroneDB.Models;
+using Registry.Web.Data;
+using Registry.Web.Data.Models;
+using Registry.Web.Models;
+using Registry.Web.Models.DTO;
+using Registry.Web.Services.Ports;
+using Registry.Web.Utilities;
+
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Auth0.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 
-public class AccountController : Controller
+namespace Registry.Web.Controllers
 {
-    public async Task Login(string returnUrl = "/")
+    [ApiController]
+
+    public class AccountController : ControllerBaseEx
     {
-        var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
-            .WithRedirectUri(returnUrl)
-            .Build();
-
-        await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
-    }
-
-    [Authorize]
-    public async Task Logout()
-    {
-        var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
-            // Indicate here where Auth0 should redirect the user after a logout.
-            // Note that the resulting absolute Uri must be added in the
-            // **Allowed Logout URLs** settings for the client.
-            .WithRedirectUri(Url.Action("Index", "Home"))
-            .Build();
-
-        await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
-        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-    }
-
-    [Authorize]
-    public IActionResult Profile()
-    {
-        return View(new
+        public async Task Login(string returnUrl = "/")
         {
-            Name = User.Identity.Name,
-            EmailAddress = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
-            ProfileImage = User.Claims.FirstOrDefault(c => c.Type == "picture")?.Value
-        });
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+                .WithRedirectUri(returnUrl)
+                .Build();
+
+            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        }
+
+        [Authorize]
+        public async Task Logout()
+        {
+            var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+                // Indicate here where Auth0 should redirect the user after a logout.
+                // Note that the resulting absolute Uri must be added in the
+                // **Allowed Logout URLs** settings for the client.
+                .WithRedirectUri(Url.Action("Index", "Home"))
+                .Build();
+
+            await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View(new
+            {
+                Name = User.Identity.Name,
+                EmailAddress = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
+                ProfileImage = User.Claims.FirstOrDefault(c => c.Type == "picture")?.Value
+            });
+        }
     }
 }
 
