3e0fa739b162b347659466dae725771bf880eda3
Owen Kaluza <owen.kaluza@monash.edu>
2022-07-14 20:58:25 +1000

    Fix route

--- Registry.Web/Controllers/AccountController.cs
+++ Registry.Web/Controllers/AccountController.cs
@@ -32,7 +32,7 @@ using Microsoft.AspNetCore.Authorization;
 namespace Registry.Web.Controllers
 {
     [ApiController]
-    [Route("login")]
+    [Route("login/auth0")]
 
     public class AccountController : Controller
     {
